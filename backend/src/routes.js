aa180e17c00c7ce9a122022b8629bfaa928cf5a1
marcelogaldino <marcelinhomgo@gmail.com>
2019-10-03 23:27:52 -0300

    Implementado o pattern MVC e rotas para USER, SPOT, DASHBOARD e BOOKING

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 const routes = express.Router();
+const upload =  multer(uploadConfig);
 
 /*
 A definição de rotas aceita a seguinte estrutura:
@@ -13,5 +19,10 @@ HANDLER é a função executada quando a rota é correspondida.
 */
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+routes.get('/dashboard', DashboardController.show);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
